08b5867aecf6135d39dd6748eb0ae4a87c48f87f
EC2 Default User <ec2-user@ip-172-31-15-116.ap-northeast-1.compute.internal>
2022-12-15 09:08:49 +0000

    12.投稿機能の作成(5)-投稿ごとの画面を作ろう

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
     post_image_
   end
-  
+
   def after_sign_out_path_for(resource)
     about_path
   end
-  
+
   #投稿データのストロングパラメータ
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
 end
